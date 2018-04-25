9242ce892966dd2c71017f913ed7e1c76f0d47d5
robertgj <robertjenssen@ozemail.com.au>
2018-04-25 21:14:36 +1000

    Update to octave-4.2.2

--- src/schurOneMPAlatticeAsq_test.mk
+++
@@ -1,5 +0,0 @@
-schurOneMPAlatticeAsq_test_FILES = \
-schurOneMPAlatticeAsq_test.m test_common.m schurOneMPAlatticeAsq.m \
-tf2schurOneMlattice.m schurOneMlattice2Abcd.oct schurOneMAPlattice2H.oct tf2pa.m \
-schurOneMscale.m spectralfactor.oct schurdecomp.oct schurexpand.oct \
-complex_zhong_inverse.oct H2Asq.m H2T.m
--- src/schurOneMPAlatticeT_test.mk
+++
@@ -1,5 +0,0 @@
-schurOneMPAlatticeT_test_FILES = \
-schurOneMPAlatticeAsq_test.m test_common.m schurOneMPAlatticeT.m \
-tf2schurOneMlattice.m schurOneMlattice2Abcd.oct schurOneMAPlattice2H.oct tf2pa.m \
-schurOneMscale.m spectralfactor.oct schurdecomp.oct schurexpand.oct \
-complex_zhong_inverse.oct H2Asq.m H2T.m
