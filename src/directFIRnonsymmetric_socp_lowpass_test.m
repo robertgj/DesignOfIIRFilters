d1c1d150cba041d43217bfaeb70089c03bde1ff3
Robert Jenssen <DesignOfIIRFilters@gmail.com>
2021-01-14 19:39:31 +1100

    Add directFIRnonsymmetric_socp_lowpass_test.m

---
+++ src/directFIRnonsymmetric_socp_lowpass_test.m
@@ -0,0 +1,125 @@
+% directFIRnonsymmetric_socp_lowpass_test.m
+% Copyright (C) 2021 Robert G. Jenssen
+% SDP design of a direct-form non-symmetric FIR lowpass filter with SOCP
+
+test_common;
+
+delete("directFIRnonsymmetric_socp_lowpass_test.diary");
+delete("directFIRnonsymmetric_socp_lowpass_test.diary.tmp");
+diary directFIRnonsymmetric_socp_lowpass_test.diary.tmp
+
+tic;
+
+verbose=false;
+strf="directFIRnonsymmetric_socp_lowpass_test";
+
+% Low-pass filter specification
+N=30;d=10;fap=0.175;Wap=1;Wat=0.01;fas=0.25;Was=100;
+
+% Set up the SeDuMi arguments for quadratic constraint on the amplitude response
+[~,~,Q,q]=directFIRnonsymmetricEsqPW ...
+            (zeros(N+1,1),[0,fap,fas,0.5]*2*pi,[1,0,0],[d,0,0],[Wap,Wat,Was]);
+[U,P]=chol(Q);
+if P~=0
+  error("Q not positive definite");
+endif
+b=[1;zeros(N+1,1)];
+At=-[b,[zeros(1,N+1);U]];
+bt=-b;
+ct=[0;inv(U')*q(:)];
+K.q=size(At,2);
+pars.fid=1;
+
+% Call SeDuMi
+try
+  [x,y,info]=sedumi(At,bt,ct,K,pars);
+  printf("SeDuMi info.iter=%d, info.feasratio=%6.4g\n",info.iter,info.feasratio);
+  if info.pinf
+    error("SeDuMi primary problem infeasible");
+  endif
+  if info.dinf
+    error("SeDuMi dual problem infeasible");
+  endif 
+  if info.numerr == 1
+    error("SeDuMi premature termination"); 
+  elseif info.numerr == 2 
+    error("SeDuMi numerical failure");
+  elseif info.numerr
+    error("SeDuMi info.numerr=%d",info.numerr);
+  endif
+catch
+  err=lasterror();
+  for e=1:length(err.stack)
+    fprintf(stderr,"Called %s at line %d\n", ...
+            err.stack(e).name,err.stack(e).line);
+  endfor
+  error("%s\n", err.message);
+end_try_catch
+
+% Extract filter impulse response
+h=y(2:end);
+print_polynomial(h,"h");
+print_polynomial(h,"h",strcat(strf,"_h_coef.m"));
+
+% Calculate response
+nplot=1000;
+nap=floor(fap*nplot/0.5)+1;
+nas=floor(fas*nplot/0.5)+1;
+f=(0:(nplot-1))'*0.5/nplot;
+w=2*pi*f;
+H=freqz(h,1,w);
+T=grpdelay(h,1,nplot);
+
+% Find pass-band ripple
+[maxPassRipple,imaxPassRipple]=max(abs(abs(H(1:nap))-1));
+dBap=abs(20*log10(abs(H(imaxPassRipple))));
+[maxStop,imaxStop]=max(abs(H(nas:end)));
+imaxStop=imaxStop+nas;
+dBas=abs(20*log10(maxStop));
+tdr=max(abs(T(1:nap)-d));
+
+% Plot response
+subplot(211)
+plot(f,20*log10(abs(H)), ...
+     f([imaxPassRipple,imaxStop]), ...
+     20*log10(abs(H([imaxPassRipple,imaxStop]))),"+");
+ylabel("Amplitude(dB)");
+axis([0 0.5 -80 10]);
+grid("on");
+strt=sprintf("SOCP designed non-symmetric FIR filter : \
+N=%d,d=%d,fap=%g,dBap=%g,tdr=%g,Wap=%g,fas=%g,dBas=%g,Was=%g",
+             N,d,fap,dBap,tdr,Wap,fas,dBas,Was);
+title(strt);
+subplot(212)
+plot(w*0.5/pi,T);
+xlabel("Frequency");
+ylabel("Delay(samples)");
+axis([0 0.5 0 20]);
+grid("on");
+print(strcat(strf,"_response"),"-dpdflatex");
+subplot(211)
+axis([0 fap -1.0 0.2]);
+subplot(212)
+axis([0 fap d+[-2,2]]);
+grid("on");
+print(strcat(strf,"_passband"),"-dpdflatex");
+close
+
+% Filter specification
+fid=fopen(strcat(strf,".spec"),"wt");
+fprintf(fid,"N=%d %% FIR filter order\n",N);
+fprintf(fid,"d=%d %% Nominal FIR filter delay\n",d);
+fprintf(fid,"fap=%g %% Amplitude pass band edge\n",fap);
+fprintf(fid,"Wap=%g %% Amplitude pass band weight\n",Wap);
+fprintf(fid,"fas=%g %% Amplitude stop band edge\n",fas);
+fprintf(fid,"Was=%g %% Amplitude stop band weight\n",Was);
+fclose(fid);
+
+% Save results
+save directFIRnonsymmetric_socp_lowpass_test.mat N d fap dBap Wap fas dBas Was h
+       
+% Done
+toc;
+diary off
+movefile directFIRnonsymmetric_socp_lowpass_test.diary.tmp ...
+         directFIRnonsymmetric_socp_lowpass_test.diary;
---
+++ test/05/t0515a.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+prog=directFIRnonsymmetric_socp_lowpass_test.m
+depends="directFIRnonsymmetric_socp_lowpass_test.m test_common.m \
+directFIRnonsymmetricEsqPW.m print_polynomial.m"
+
+tmp=/tmp/$$
+here=`pwd`
+if [ $? -ne 0 ]; then echo "Failed pwd"; exit 1; fi
+
+fail()
+{
+        echo FAILED ${0#$here"/"} $prog 1>&2
+        cd $here
+        rm -rf $tmp
+        exit 1
+}
+
+pass()
+{
+        echo PASSED ${0#$here"/"} $prog
+        cd $here
+        rm -rf $tmp
+        exit 0
+}
+
+trap "fail" 1 2 3 15
+
+mkdir $tmp
+if [ $? -ne 0 ]; then echo "Failed mkdir"; exit 1; fi
+for file in $depends;do \
+  cp -R src/$file $tmp; \
+  if [ $? -ne 0 ]; then echo "Failed cp "$file; fail; fi \
+done
+cd $tmp
+if [ $? -ne 0 ]; then echo "Failed cd"; fail; fi
+
+#
+# the output should look like this
+#
+cat > test_h_coef.ok << 'EOF'
+h = [  -0.0042477821,  -0.0100321651,  -0.0055113226,   0.0139949470, ... 
+        0.0257508711,  -0.0019173741,  -0.0535366135,  -0.0483407796, ... 
+        0.0812247585,   0.2838583335,   0.4006985881,   0.3186391905, ... 
+        0.1007575061,  -0.0709997118,  -0.0845585932,   0.0021185804, ... 
+        0.0562731786,   0.0269180573,  -0.0254789957,  -0.0320020865, ... 
+        0.0013673636,   0.0230306848,   0.0110861041,  -0.0097336498, ... 
+       -0.0124726072,  -0.0004610972,   0.0074359811,   0.0044293255, ... 
+       -0.0015263431,  -0.0031482038,  -0.0014018788 ]';
+EOF
+if [ $? -ne 0 ]; then echo "Failed output cat test_h_coef.ok"; fail; fi
+
+#
+# run and see if the results match. 
+#
+echo "Running $prog"
+
+octave-cli -q $prog >test.out 2>&1
+if [ $? -ne 0 ]; then echo "Failed running $prog"; fail; fi
+
+diff -bB test_h_coef.ok directFIRnonsymmetric_socp_lowpass_test_h_coef.m
+if [ $? -ne 0 ]; then echo "Failed diff -Bb test_h_coef.ok"; fail; fi
+
+#
+# this much worked
+#
+pass
+
