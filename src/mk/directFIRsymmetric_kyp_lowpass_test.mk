9d0bb9106f55a14dae612d2704f4a75a37c4cf2c
Robert Jenssen <DesignOfIIRFilters@gmail.com>
2023-06-29 17:03:20 +1000

    Add Add Parks-McClellan comparison to directFIRsymmetric_kyp_lowpass_test.m

--- src/mk/directFIRsymmetric_kyp_lowpass_test.mk
+++ src/mk/directFIRsymmetric_kyp_lowpass_test.mk
@@ -1,12 +1,15 @@
 directFIRsymmetric_kyp_lowpass_test_FIGURES = \
-directFIRsymmetric_kyp_lowpass_test_response 
+directFIRsymmetric_kyp_lowpass_test_response \
+directFIRsymmetric_kyp_lowpass_test_mcclellan
 
 directFIRsymmetric_kyp_lowpass_test_COEFS = \
 directFIRsymmetric_kyp_lowpass_test.mat \
 directFIRsymmetric_kyp_lowpass_test.spec \
 directFIRsymmetric_kyp_lowpass_test_h_coef.m \
 directFIRsymmetric_kyp_lowpass_test_max_passband_squared_error.tab \
-directFIRsymmetric_kyp_lowpass_test_max_stopband_squared_error.tab
+directFIRsymmetric_kyp_lowpass_test_max_stopband_squared_error.tab \
+directFIRsymmetric_kyp_lowpass_test_mcclellan.spec \
+directFIRsymmetric_kyp_lowpass_test_mcclellan_hM_coef.m
 
 directFIRsymmetric_kyp_lowpass_test_FILES = \
 directFIRsymmetric_kyp_lowpass_test.m test_common.m \
--- src/test/directFIRsymmetric_kyp_lowpass_test.m
+++ src/test/directFIRsymmetric_kyp_lowpass_test.m
@@ -1,12 +1,12 @@
 % directFIRsymmetric_kyp_lowpass_test.m
 %
-% Copyright (C) 2022 Robert G. Jenssen
+% Copyright (C) 2022-2023 Robert G. Jenssen
 
-% SDP design of a direct-form FIR lowpass filter with the KYP lemma.
-% See Section VII.B.2, pp. 53-55 of "Generalised KYP Lemma: Unified
-% Frequency Domain Inequalities With Design Applications", T. Iwasaki
-% and S. Hara, IEEE Transactions on Automatic Control, Vol. 50, No. 1,
-% January 2005, pp. 41–59
+% SDP design of a symmetric direct-form FIR lowpass filter with the
+% KYP lemma. See Section VII.B.2, pp. 53-55 of "Generalised KYP Lemma:
+% Unified Frequency Domain Inequalities With Design Applications",
+% T. Iwasaki and S. Hara, IEEE Transactions on Automatic Control,
+% Vol. 50, No. 1, January 2005, pp. 41–59
 
 test_common;
 
@@ -16,9 +16,14 @@ delete(strcat(strf,".diary"));
 delete(strcat(strf,".diary.tmp"));
 eval(sprintf("diary %s.diary.tmp",strf));
 
-% Low-pass filter specification (Reduce Esq_z while satisfying the constraints)
-M=15;N=2*M;fap=0.15;fas=0.2;Esq_s=1e-4;
-d=M;Esq_z=3.24e-3;
+tic;
+
+%
+% Low-pass filter
+%
+M=27;N=2*M;d=M;
+fap=0.15;Esq_z=0.81241e-5;
+fas=0.2;Esq_s=1e-5;
 
 % Common constants
 A=[zeros(N-1,1),eye(N-1);zeros(1,N)];
@@ -85,56 +90,100 @@ nplot=1000;
 nap=(fap*nplot/0.5)+1;
 nas=(fas*nplot/0.5)+1;
 h=value(fliplr(CD));
-w=(0:(nplot-1))'*pi/nplot;
+f=(0:(nplot-1))'*0.5/nplot;
+w=f*2*pi;
 H=freqz(h,1,w);
 ax=plotyy(w(1:nap)*0.5/pi,20*log10(abs(H(1:nap))), ...
           w(nas:end)*0.5/pi,20*log10(abs(H(nas:end))));
 set(ax(1),"ycolor","black");
 set(ax(2),"ycolor","black");
-axis(ax(1),[0 0.5 -1 1]);
-axis(ax(2),[0 0.5 -60 -20]);
+axis(ax(1),[0 0.5 -0.04 0.04]);
+axis(ax(2),[0 0.5 -60 -40]);
 ylabel("Amplitude(dB)");
 xlabel("Frequency");
 grid("on");
-strt=sprintf("N=%d,d=%d,fap=%4.2f,fas=%4.2f,Esq\\_z=%8.4g,Esq\\_s=%8.4g",
-             N,d,fap,fas,Esq_z,Esq_s);
+strt=sprintf("N=%d,d=%d,fap=%4.2f,Esq\\_z=%g,fas=%4.2f,Esq\\_s=%g",
+             N,d,fap,Esq_z,fas,Esq_s);
 title(strt);
 print(strcat(strf,"_response"),"-dpdflatex");
 close
 
 % Check amplitude response
 max_Esq_z=max(abs(H(1:nap)-e.^(-j*w(1:nap)*d)))^2;
-printf("max_Esq_z=%10.8f\n",max_Esq_z);
+printf("max_Esq_z=%g\n",max_Esq_z);
 fid=fopen(strcat(strf,"_max_passband_squared_error.tab"),"wt");
-fprintf(fid,"%10.8f",max_Esq_z);
+fprintf(fid,"%10.5g",max_Esq_z);
 fclose(fid);
 max_Esq_s=max(abs(H(nas:end)))^2;
-printf("max_Esq_s=%10.8f\n",max_Esq_s);
+printf("max_Esq_s=%g\n",max_Esq_s);
 fid=fopen(strcat(strf,"_max_stopband_squared_error.tab"),"wt");
-fprintf(fid,"%10.8f",max_Esq_s);
+fprintf(fid,"%10.5g",max_Esq_s);
 fclose(fid);
-printf("min_A_p=%10.8f\n",min(abs(H(1:nap))));
-printf("max_A_t=%10.8f\n",max(abs(H(nap:nas))));
-printf("max_A_s=%10.8f\n",max(abs(H(nas:end))));
+printf("max_A_p=%10.8f, %5.3fdB\n",
+       max(abs(H(1:nap))),20*log10(max(abs(H(1:nap)))));
+printf("min_A_p=%10.8f, %5.3fdB\n",
+       min(abs(H(1:nap))),20*log10(max(abs(H(1:nap)))));
+printf("max_A_t=%10.8f, %5.3fdB\n",
+       max(abs(H(nap:nas))),20*log10(max(abs(H(nap:nas)))));
+printf("max_A_s=%10.8f, %5.3fdB\n",
+       max(abs(H(nas:end))),20*log10(max(abs(H(nas:end)))));
 % Overall squared-error
 Esq=directFIRsymmetricEsqPW(h(1:(M+1)),[0,fap,fas,0.5]*2*pi,[1,0,0],[1,1,1]);
 printf("Esq=%10.8f\n",Esq);
 
 % Save results
 fid=fopen(strcat(strf,".spec"),"wt");
-fprintf(fid,"N=%d %% FIR filter order\n",N);
-fprintf(fid,"d=%d %% Nominal FIR filter delay\n",d);
+fprintf(fid,"M=%d %% Filter order is 2*M\n",M);
 fprintf(fid,"fap=%g %% Amplitude pass band edge\n",fap);
 fprintf(fid,"fas=%g %% Amplitude stop band edge\n",fas);
 fprintf(fid,"Esq_z=%g %% Squared pass band error from delay\n",Esq_z);
 fprintf(fid,"Esq_s=%g %% Squared amplitude stop band error\n",Esq_s);
 fclose(fid);
-
 print_polynomial(h,"h","%13.10f");
 print_polynomial(h,"h",strcat(strf,"_h_coef.m"),"%13.10f");
-
 eval(sprintf("save %s.mat N d fap fas Esq_z Esq_s h",strf));
 
+%
+% Comparison with mcclellanFIRsymmetric
+%
+F=[f(1:(nap-1));fap;fas;f((nas+1):end)];
+D=[ones(nap,1); zeros(length(F)-nap,1)];
+K=1;
+W=[ones(nap,1)/K; ones(length(F)-nap,1)];
+[hM,rho,fext,fiter,feasible]=mcclellanFIRsymmetric(M,F,D,W);
+if feasible==false
+  error("hM not feasible");
+endif
+wa=(0:nplot)'*pi/nplot;
+A=directFIRsymmetricA(wa,hM);
+ax=plotyy(wa(1:nap)*0.5/pi,20*log10(abs(A(1:nap))), ...
+          wa(nas:end)*0.5/pi,20*log10(abs(A(nas:end))));
+set(ax(1),'ycolor','black');
+set(ax(2),'ycolor','black');
+axis(ax(1),[0 0.5 -0.04 0.04]);
+axis(ax(2),[0 0.5 -60 -40]);
+strt=sprintf("McClellan lowpass FIR: M=%d,fap=%g,fas=%g,K=%g,rho=%g", ...
+             M,fap,fas,K,rho);
+title(strt);
+ylabel("Amplitude(dB)");
+xlabel("Frequency");
+grid("on");
+print(strcat(strf,"_mcclellan"),"-dpdflatex");
+close
+fid=fopen(strcat(strf,"_mcclellan.spec"),"wt");
+fprintf(fid,"M=%d %% Filter order is 2*M\n",M);
+fprintf(fid,"fap=%g %% Amplitude pass band edge\n",fap);
+fprintf(fid,"fas=%g %% Amplitude stop band edge\n",fas);
+fprintf(fid,"K=%d %% Stop band weight\n",K);
+fprintf(fid,"nplot=%d %% Number of frequency grid points in [0,0.5]\n",nplot);
+fclose(fid);
+print_polynomial(hM,"hM","%13.10f");
+print_polynomial(hM,"hM",strcat(strf,"_mcclellan_hM_coef.m"),"%13.10f");
+
+
+%
 % Done
+%
+toc;
 diary off
 movefile(strcat(strf,".diary.tmp"),strcat(strf,".diary"),"f");
