96a72506d0d5c41984be6fc0986ec0de12242a76
Robert Jenssen <DesignOfIIRFilters@gmail.com>
2025-06-05 07:24:30 +1000

    Add dAdx etc to tf2Abcd

--- src/test/tf2Abcd_test.m
+++ src/test/tf2Abcd_test.m
@@ -1,18 +1,150 @@
-% tf2Abcd_test.m
+% tf2Abcd_test .m
 % Copyright (C) 2017-2025 Robert G. Jenssen
 
 test_common;
 
-delete("tf2Abcd_test.diary");
-delete("tf2Abcd_test.diary.tmp");
-diary tf2Abcd_test.diary.tmp
+strf="tf2Abcd_test";
 
+delete(strcat(strf,".diary"));
+delete(strcat(strf,".diary.tmp"));
+eval(sprintf("diary %s.diary.tmp",strf));
 
-fc=0.05
-[n,d]=butter(3,2*fc)
-[Aa,ba,ca,da]=tf2Abcd(n,d)
-[Ab,bb,cb,db]=tf2Abcd(n,1)
-[Ac,bc,cc,dc]=tf2Abcd(1,d)
+NN=5;
+fc=0.05;
+[N,D]=butter(NN,2*fc);
 
+%
+% Check arguments
+try
+  [A,b,c,d]=tf2Abcd();
+catch
+  fprintf(stderr,"Caught nargin==0!\n");
+end_try_catch
+try
+  [A,b,c,d]=tf2Abcd(N);
+catch
+  fprintf(stderr,"Caught nargin==1!\n");
+end_try_catch
+try
+  [A,b,c,d]=tf2Abcd(N,D,B);
+catch
+  fprintf(stderr,"Caught nargin==3!\n");
+end_try_catch
+try
+  tf2Abcd(N,D);
+catch
+  fprintf(stderr,"Caught nargout==0!\n");
+end_try_catch
+try
+  [A,b,c]=tf2Abcd(N,D);
+catch
+  fprintf(stderr,"Caught nargout==3!\n");
+end_try_catch
+try
+  [A,b,c,d,e]=tf2Abcd(N,D);
+catch
+  fprintf(stderr,"Caught nargout==5!\n");
+end_try_catch
+try
+  [A,b,c,d,e,f]=tf2Abcd(N,D);
+catch
+  fprintf(stderr,"Caught nargout==6!\n");
+end_try_catch
+try
+  [A,b,c,d,e,f,g]=tf2Abcd(N,D);
+catch
+  fprintf(stderr,"Caught nargout==7!\n");
+end_try_catch
+try
+  [A,b,c,d,e,f,g,h,i]=tf2Abcd(N,D);
+catch
+  fprintf(stderr,"Caught nargout==9!\n");
+end_try_catch
+try
+  [A,b,c,d]=tf2Abcd([],1);
+catch
+  fprintf(stderr,"Caught N is empty!\n");
+end_try_catch
+try
+  [A,b,c,d]=tf2Abcd(1,[]);
+catch
+  fprintf(stderr,"Caught D is empty!\n");
+end_try_catch
+
+[A,b,c,d]=tf2Abcd(3,2)
+[A,b,c,d,dAdx,dbdx,dcdx,dddx]=tf2Abcd(3,2)
+
+% Check results
+f=cell(1,5);
+f{1}.N=N;      f{1}.D=D;
+f{2}.N=[1];    f{2}.D=D;
+f{3}.N=N;      f{3}.D=[1];
+f{4}.N=N(1:3); f{4}.D=D;
+f{5}.N=N;      f{5}.D=D(1:3);
+
+tol=1e-8;
+del=1e-6;
+
+for k=1:length(f)
+  
+  printf("k=%d\n",k);
+  printf("N=");
+  disp(f{k}.N);
+  printf("D=");
+  disp(f{k}.D);
+
+  [A,b,c,d]=tf2Abcd(f{k}.N,f{k}.D)
+  [~,~,~,~,dAdx,dbdx,dcdx,dddx]=tf2Abcd(f{k}.N,f{k}.D)
+
+  deln=zeros(size(f{k}.N));
+  deln(1)=del/2;
+  for l=1:length(f{k}.N)
+    [AP,bP,cP,dP]=tf2Abcd(f{k}.N+deln,f{k}.D);
+    [AN,bN,cN,dN]=tf2Abcd(f{k}.N-deln,f{k}.D);
+    deln=circshift(deln,1);
+    est_dAdx=(AP-AN)/del;
+    if max(abs(abs(est_dAdx-dAdx{l}))) > tol
+      error("max(abs(abs(est_dAdx-dAdx{l}))) > tol");
+    endif
+    est_dbdx=(bP-bN)/del;
+    if max(abs(est_dbdx-dbdx{l})) > tol
+      error("max(abs(est_dbdx-dbdx{l})) > tol");
+    endif
+    est_dcdx=(cP-cN)/del;
+    if max(abs(est_dcdx-dcdx{l})) > tol
+      error("max(abs(est_dcdx-dcdx{l})) > tol");
+    endif
+    est_dddx=(dP-dN)/del;
+    if max(abs(est_dddx-dddx{l})) > tol
+      error("max(abs(est_dddx-dddx{l})) > tol");
+    endif
+  endfor
+
+  deld=zeros(size(f{k}.D));
+  deld(2)=del/2;
+  for l=1:(length(f{k}.D)-1)
+    [AP,bP,cP,dP]=tf2Abcd(f{k}.N,f{k}.D+deld);
+    [AN,bN,cN,dN]=tf2Abcd(f{k}.N,f{k}.D-deld);
+    deld=circshift(deld,1);
+    est_dAdx=(AP-AN)/del;
+    if max(abs(abs(est_dAdx-dAdx{length(f{k}.N)+l}))) > tol
+      error("max(abs(abs(est_dAdx-dAdx{length(f{k}.N)+l}))) > tol");
+    endif
+    est_dbdx=(bP-bN)/del;
+    if max(abs(est_dbdx-dbdx{length(f{k}.N)+l})) > tol
+      error("max(abs(est_dbdx-dbdx{length(f{k}.N)+l})) > tol");
+    endif
+    est_dcdx=(cP-cN)/del;
+    if max(abs(est_dcdx-dcdx{length(f{k}.N)+l})) > tol
+      error("max(abs(est_dcdx-dcdx{length(f{k}.N)+l})) > tol");
+    endif
+    est_dddx=(dP-dN)/del;
+    if max(abs(est_dddx-dddx{length(f{k}.N)+l})) > tol
+      error("max(abs(est_dddx-dddx{length(f{k}.N)+l})) > tol");
+    endif
+  endfor
+endfor
+
+% Done
 diary off
-movefile tf2Abcd_test.diary.tmp tf2Abcd_test.diary;
+movefile(strcat(strf,".diary.tmp"),strcat(strf,".diary"));
--- src/tf2Abcd.m
+++ src/tf2Abcd.m
@@ -1,10 +1,17 @@
-function [A,b,c,d]=tf2Abcd(N,D)
-% [A,b,c,d]=tf2Abcd(N,D)
-% Convert the transfer function H(z)=N(z)/D(z) to state-variable form
-% D(z)=z^n+D(2)z^(n-1)+...+D(n-1)z+D(n)
-% N(z)=N(1)z^n+...+N(n-1)z+N(n)
-% x(k+1)=Ax(k)+bu(k), y(k)=cx(k)+du(k)
-
+function [A,b,c,d,dAdx,dbdx,dcdx,dddx]=tf2Abcd(N,D)
+% [A,b,c,d,dAdx,dbdx,dcdx,dddx]=tf2Abcd(N,D)
+% Convert the direct form transfer function, H(z)=N(z)/D(z), where:
+%    D(z)=z^n+D(2)z^(n-1)+...+D(n-1)z+D(n)
+%    N(z)=N(1)z^n+...+N(n-1)z+N(n)
+%
+% to the state-variable form:
+%    x(k+1)=Ax(k)+bu(k)
+%      y(k)=cx(k)+du(k)
+%
+% The dAdx etc are cell arrays of length length(N)+length(D)-1 corresponding
+% to the gradients of A etc with respect to N(1),...,N(end),D(2),...,D(end)
+% where I assume D(1)=1.
+  
 % Copyright (C) 2017-2025 Robert G. Jenssen
 %
 % Permission is hereby granted, free of charge, to any person
@@ -25,9 +32,20 @@ function [A,b,c,d]=tf2Abcd(N,D)
 % TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
 % SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+  if (nargin ~= 2) || ((nargout ~= 4) && (nargout ~=8))
+    print_usage(["[A,b,c,d]=tf2Abcd(N,D)\n", ...
+                 "[A,b,c,d,dAdx,dbdx,dcdx,dddx]=tf2Abcd(N,D)"]);
+  endif
+  if isempty(N)
+    error("N is empty");
+  endif
+  if isempty(D)
+    error("D is empty");
+  endif
+  
   N=N(:)'/D(1);
   D=D(:)'/D(1);
-
+  
   nD=length(D);
   nN=length(N);
   if nD>nN
@@ -40,10 +58,62 @@ function [A,b,c,d]=tf2Abcd(N,D)
     n=nN;
   endif
   
-  A=[zeros(n-2,1) eye(n-2);-D(n:-1:2)];
-  b=[zeros(n-2,1);1];
-  c=N(n:-1:2)-(N(1)*D(n:-1:2));
-  d=N(1);
+  if (length(N) == 1) && (length(D) == 1)
+    A=[];
+    b=[];
+    c=[];
+    d=N(1);
+  else
+    A=[zeros(n-2,1) eye(n-2);-D(n:-1:2)];
+    b=[zeros(n-2,1);1];
+    c=N(n:-1:2)-(N(1)*D(n:-1:2));
+    d=N(1);
+  endif
 
+  if nargout == 4
+    return;
+  endif
+
+  nND=nN+nD-1;
+  dAdx=cell(1,nND);
+  dbdx=cell(1,nND);
+  dcdx=cell(1,nND);
+  dddx=cell(1,nND);
+
+  if (length(N) == 1) && (length(D) == 1)
+    dAdx{1}=[];
+    dbdx{1}=[];
+    dcdx{1}=[];
+    dddx{1}=1;
+    return;
+  endif
+  
+  for k=1:nN,
+    dAdx{k}=zeros(size(A));
+  endfor   
+  for k=2:nD,
+    dAdx{nN+k-1}=zeros(size(A));
+    dAdx{nN+k-1}(n-1,n-k+1)=-1;
+  endfor
+
+  for k=1:nND,
+    dbdx{k}=zeros(size(b));
+  endfor
+
+  dcdx{1}=-D(n:-1:2);
+  for k=2:nN,
+    dcdx{k}=zeros(size(c));
+    dcdx{k}(n-k+1)=1;
+  endfor
+  for k=2:nD,
+    dcdx{nN+k-1}=zeros(size(c));
+    dcdx{nN+k-1}(n-k+1)=-N(1);
+  endfor
+
+  dddx{1}=1;
+  for k=2:nND,
+    dddx{k}=0;
+  endfor
+  
 endfunction
 
